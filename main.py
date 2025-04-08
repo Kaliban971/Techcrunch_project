49cbb81fff0cffea4557a1133b4cb97b14385946
Kaliban971 <afrokraibonaut@disroot.org>
2025-04-08 16:37:44 +0200

    ajout de l'extraction du texte des articles et timme.sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from parsel import Selector
 import requests
-#import polars as pl
 from icecream import ic
 import json
 import time
@@ -21,32 +20,41 @@ selector_accueil = Selector(text=response_accueil.text) # Utiliser Selector pour
 
 # Extraire chaque carte d'article
 articles = []
-for card in selector_accueil.css('div.loop-card'):
-    categorie = card.css('a.loop-card__cat::text').get()
+for card in selector_accueil.css('div.loop-card'): # boucle pour chaque carte d'article
+    categorie = card.css('a.loop-card__cat::text').get() or card.css('span.loop-card__cat::text').get()
     categorie_url = card.css('a.loop-card__cat::attr(href)').get()
     titre = card.css('a.loop-card__title-link::text').get()
     lien = card.css('a.loop-card__title-link::attr(href)').get()
-    auteur = card.css('a.loop-card__author::text').get()
+    auteur = card.css('a.loop-card__author::text').get() or card.css('a.loop-card__author:text').get()
     date_relative = card.css('time.loop-card__time::text').get()
     date_absolue = card.css('time.loop-card__time::attr(datetime)').get()
-   
+
+
+    time.sleep(2)
+
+
+#Extraire le texte des articles
+    response_article = requests.get(lien, headers=headers)
+    selector_article = Selector(response_article.text)
+
+    summary_article = selector_article.css('p#speakable-summary.wp-block-paragraph').get()
+    text_article = selector_article.css('p.wp-block-paragraph').getall()
+
+
+
 
 
     articles.append({
-        'Catégorie': categorie,
-        'URL_Catégorie': categorie_url,
+        'Categorie': categorie,
+        'URL_Categorie': categorie_url,
         'Titre': titre,
         'URL_Article': lien,
-        'texte': texte_article,
         'Auteur': auteur,
         'Date_relative': date_relative,
-        'Date_absolue': date_absolue
+        'Date_absolue': date_absolue,
+        'Summary_article': summary_article,
+        'Text_article': text_article
     })
-
-
-
-# Afficher le premier article
-#ic(articles[1])
 
 
 
@@ -62,4 +70,3 @@ for article in articles:
 # Écrire dans un fichier JSON
 with open("profil.json", "w", encoding="utf-8") as fichier: # le fichier est ouvert en mode écriture ("w")
     json.dump(articles, fichier, indent=4) # les données sont écrites dans le fichier en format JSON, avec une indentation de 4 espaces pour une meilleure lisibilité.
-
