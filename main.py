fe6150063d26cc98209b0febb79abbafd7c54e38
Kaliban971 <afrokraibonaut@disroot.org>
2025-04-09 19:00:36 +0200

    Ajout: vérificcation de requête

--- main.py
+++ main.py
@@ -13,18 +13,25 @@ headers = {
 
 
 # Pour trouver les cartes d'articles sur la page d'accueil
-url_accueil = "https://techcrunch.com/latest" # Remplacez par l'URL de la page d'accueil de TechCrunch
+url_accueil = "https://techcrunch.com/latest/" # Remplacez par l'URL de la page d'accueil de TechCrunch
 response_accueil = requests.get(url_accueil, headers=headers) # Ajouter les headers
+
+# Vérifier si la requête a réussi
+if response_accueil.status_code == 200:
+    ic("Requête réussie")
+else:
+    ic("Erreur lors de la requête")
+
 selector_accueil = Selector(text=response_accueil.text) # Utiliser Selector pour analyser le HTML
 
 
 # Extraire chaque carte d'article
 articles = []
 for card in selector_accueil.css('div.loop-card'): # boucle pour chaque carte d'article
-    categorie = card.css('a.loop-card__cat::text').get() or card.css('span.loop-card__cat::text').get()
-    categorie_url = card.css('a.loop-card__cat::attr(href)').get()
-    titre = card.css('a.loop-card__title-link::text').get()
+    categorie = card.css('.loop-card__cat::text').get() or card.css('span.loop-card__cat::text').get()
+    categorie_url = card.css('a.loop-card__title-link::attr(href)').get()
     lien = card.css('a.loop-card__title-link::attr(href)').get()
+    titre = card.css('a.loop-card__title-link::text').get()
     auteur = card.css('a.loop-card__author::text').get() or card.css('a.loop-card__author:text').get()
     date_relative = card.css('time.loop-card__time::text').get()
     date_absolue = card.css('time.loop-card__time::attr(datetime)').get()
@@ -37,23 +44,20 @@ for card in selector_accueil.css('div.loop-card'): # boucle pour chaque carte d'
     response_article = requests.get(lien, headers=headers)
     selector_article = Selector(response_article.text)
 
-    summary_article =selector_article.css('p#speakable-summary.wp-block-paragraph::text').getall() #sommaire de l'article
-    text_article = selector_article.css('p.wp-block-paragraph::text').get()
+    full_article = selector_article.css('p.wp-block-paragraph::text').getall() #texte complet de l'article
 
 
 
-
-
+    # Ajouter les informations de l'article à la liste
     articles.append({
         'Categorie': categorie,
         'URL_Categorie': categorie_url,
-        'Titre': titre,
         'URL_Article': lien,
+        'Titre': titre,
         'Auteur': auteur,
         'Date_relative': date_relative,
         'Date_absolue': date_absolue,
-        'Summary_article': summary_article,
-        'Text_article': text_article
+        'Full_article': full_article
     })
 
 
@@ -66,9 +70,9 @@ ic(articles[0])
 
 
 
-
+#TODO:Voir comment faire pour gerer la pagination et récupérer les articles de toutes les pages
 
 
 # Écrire dans un fichier JSON
 with open("profil.json", "w", encoding="utf-8") as fichier: # le fichier est ouvert en mode écriture ("w")
-    json.dump(articles[0], fichier, ensure_ascii=False, indent=4) # les données sont écrites dans le fichier en format JSON, avec une indentation de 4 espaces pour une meilleure lisibilité et ensure_ascii=False pour garantir que les caractères non-ASCII sont encodés correctement.
+    json.dump(articles, fichier, ensure_ascii=False, indent=4) # les données sont écrites dans le fichier en format JSON, avec une indentation de 4 espaces pour une meilleure lisibilité et ensure_ascii=False pour garantir que les caractères non-ASCII sont encodés correctement.
