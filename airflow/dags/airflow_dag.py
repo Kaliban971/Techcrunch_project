2ec7f3b554c73a7863cb2de6509f1c1ad94282de
Kaliban971 <afrokraibonaut@disroot.org>
2025-04-18 17:54:33 +0200

    finalisation du script d'insertion des données dans la db modification: du script de airflow

--- airflow/dags/airflow_dag.py
+++ airflow/dags/airflow_dag.py
@@ -1,7 +1,10 @@
+import re
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from src.scrapper.main import scrape_techcrunch
+from src.db.database import init_db, insert_article
+
 
 # Définir les arguments par défaut pour le DAG
 default_arg = {
@@ -22,15 +25,40 @@ dag = DAG(
     schedule=timedelta(days=1), #Executer tous les jours
 )
 
-# Définir la tâche de scraping
+# Fonction pour initialiser la base de données
+def init_database():
+    init_db("/opt/airflow/dags/techcrunch.db")
+
+
+def scrape_and_store_article():
+    articles = scrape_techcrunch()
+    for articles in articles:
+        insert_article(articles, "/opt/airflow/dags/techcrunch.db")
+    return len(articles)
+
+
+# Définir la tâche d'initialisation de la base de données
+init_db_task = PythonOperator(
+    task_id='init_database',
+    python_callable=init_database,
+    dag=dag,
+)
+
+
+
+# Définir la tâche de scraping et stockage
 scrape_task = PythonOperator(
     task_id='scrape_techcrunch_articles',
     python_callable=scrape_techcrunch,
-    op_kwargs={'output_file':'/opt/airflow/dags/techcrunch_articles.json'},
     dag=dag,
 )
+
+# definir l'ordre des tâches
+init_db_task >> scrape_task
+
 
 #NOTE:Imported the scrape_techcrunch function from main.py
 #NOTE Set up a basic DAG with default arguments
 #NOTE Created a PythonOperator task that calls the scrape_techcrunch function
 #NOTE Added the ability to specify the output file path
+#TODO:Finir la compréension du code de airflow
--- db/database.py
+++ db/database.py
@@ -1,24 +1,53 @@
-import sqlite3
+import sqlite3 # importation de la bibliothèque sqlite3
 from icecream import ic
 
+
 #connexion à la base de données (techcrunch.db) ou création si elle n'existe pas
-conn = sqlite3.connect('techcrunch.db')
-cursor = conn.cursor()
+def init_db(db_path = 'techcrunch.db'):
+    conn = sqlite3.connect('db_path')
+    cursor = conn.cursor()
 
-with open('schema.sql', 'r') as schema_file:
-    schema = schema_file.read()
+    with open('db/schema.sql', 'r') as schema_file:
+        schema = schema_file.read()
+        cursor.executescript(schema)
 
-    cursor.executescript(schema)
+    conn.commit()
+    conn.close()
+    ic("Base de données créée avec succès")
 
-conn.commit()
-conn.close()
-ic("Base de données créée avec succès")
 
-def insert_articles(article_data, db_path = 'techcrunch.db'):
+#fonction pour insérer les articles dans la base de données
+def insert_article(article_data, db_path = 'techcrunch.db'):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
+    #nous insérons l' auteur de l'article dans la table categories s'il n'existe pas déjà.Ensuite, nous récupérons l'ID de l'auteur en utilisant son nom.
 
-cursor.execute(
-    "INSERT OR IGNORE INTO authors (author_name) VALUES (?)", (article_data['Auteur'],)
+    cursor.execute(
+    "INSERT OR IGNORE INTO authors (author_name) VALUES (?)", (article_data['Auteur'],))
+    cursor.execute(
+        "SELECT author_id FROM authors WHERE author_name = (?)", (article_data['Auteur'],))
+    author_id = cursor.fetchone()[0]
+
+    #nous insérons la catégorie de l'article dans la table categories s'il n'existe pas déjà.Ensuite, nous récupérons l'ID de la catégorie en utilisant son nom.
+    cursor.exectute(
+        "INSERT OR IGNORE INTO  categories (category_name, category_url) VALUES (?, ?)", (article_data['Categorie'], article_data['URl_categorie']))
+    cursor.execute(
+        "SELECT category_id FROM categories WHERE category_name = (?)", (article_data['Categorie'],))
+    category_id = cursor.fetchone()[0]
+
+    #inserer l'article et toutes les informations, y compris  les clés étrangères de l'autheur et de la catégorie
+    cursor.execute(
+        "INSERT OR IGNORE INTO articles (title,url, date_relative, date_absolute, author_id,category_id) VALUES (?,?,?,?,?,?)",
+        (article_data['Title'],
+        article_data['URL'],
+        article_data['Date_relative'],
+        article_data['Date_absolue'],
+        author_id,
+        category_id
+        )
     )
+
+# sauvegardons les modifications et fermons la connexion à la base de données.
+    conn.commit()
+    conn.close()
